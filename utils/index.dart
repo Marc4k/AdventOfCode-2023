6b3f4b23f3044674c1670cf97add2665951030dc
Marc <marc.dev10@proton.me>
2023-11-30 18:33:12 +0100

    refactor: removed field.dart

--- utils/index.dart
+++ utils/index.dart
@@ -5,7 +5,6 @@ export 'package:quiver/iterables.dart';
 export 'package:quiver/strings.dart';
 export 'package:tuple/tuple.dart';
 
-export 'field.dart';
 export 'generic_day.dart';
 export 'input_util.dart';
 export 'parse_util.dart';
